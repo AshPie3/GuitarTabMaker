7fa38f998b265c066ef902d5b0af5e56480757a3
ashiPie <wonky.future@gmail.com>
2025-03-28 13:37:25 +0100

    Uploading

--- src/GuitarTabMaker/GUIWindows/StartWindow.java
+++ src/GuitarTabMaker/GUIWindows/StartWindow.java
@@ -4,13 +4,19 @@ import javax.swing.*;
 import java.awt.*;
 import javax.swing.JComponent;
 
-public class TestPage {
-    //static int height = (int) (screenSize.getHeight()*0.8);
+public class StartWindow {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("My first JFrame");
+        MyButton button = new MyButton("Hello, World!");
+
+        JFrame frame = new JFrame("Guitar Tab Maker");
         frame.setSize((int) (Window.width*0.8), (int) (Window.height*0.8));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.getContentPane().add(new ShapeDrawing ());
+        Container contentPane = frame.getContentPane();
+        contentPane.setLayout(new FlowLayout());
+        contentPane.add(button);
         frame.setVisible(true);
     }
 }
+
+
+
