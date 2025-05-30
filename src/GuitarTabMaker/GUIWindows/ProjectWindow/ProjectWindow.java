79ff78d33bde6624ceb701d95df6609a59dd62c0
ashiPie <wonky.future@gmail.com>
2025-05-30 13:39:23 +0200

    Scroll automatically

--- src/GuitarTabMaker/GUIWindows/ProjectWindow/ProjectWindow.java
+++ src/GuitarTabMaker/GUIWindows/ProjectWindow/ProjectWindow.java
@@ -14,8 +14,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 import java.util.List;
 
 public class ProjectWindow {
@@ -35,6 +34,9 @@ public class ProjectWindow {
     private JScrollPane scrollFrame = new JScrollPane(); // tablature text area scroll frame tablatureTextArea(), JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
     private JPanel fretboardNumsPanel = new JPanel();
     private JPanel functionsPanel = new JPanel();
+    private JMenu openProjectM = new JMenu();
+    private HashMap<String, Integer> openProjectMap = new HashMap<>();
+    private int scrollBar_val = 0;
     
     public ProjectWindow(Project project) {
         Fretboard fretboard = project.getFretboard();
@@ -97,18 +99,18 @@ public class ProjectWindow {
     private Component TabScrollFramePanel() {
         int y = (int) (windowHeight*0.15);
         int width = (int) (windowWidth * 0.9);
-        int height = (int) (windowHeight /3.5);
-        scrollFrame = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED); // tablature text area scroll frame
+        int height = (int) (windowHeight /3);
+        scrollFrame = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); // tablature text area scroll frame
         scrollFrame.setBounds((int) (windowWidth * 0.05), y, width, height);
         scrollFrame.setBorder(null);
 
         JScrollBar scrollBar = scrollFrame.getVerticalScrollBar();
+        scrollBar.setValue(scrollBar_val);
         scrollBar.setOrientation(JScrollBar.VERTICAL);
 
         return scrollFrame;
     }
     private JTextArea tablatureTextArea() {
-
         textArea.setLayout(null);
         int width = (int) (windowWidth * 0.9);
         int height = (int) (windowHeight / 2.3);
@@ -130,28 +132,30 @@ public class ProjectWindow {
         menuPanel.setBounds(0, 0, width, height);
         menuPanel.setBackground(Window.menu_panel_c);
         menuPanel.setBorderPainted(false);
+        int i_width = width/10;
+        int i_height = height;
         int btn_num = 6;
         int[] func_x = new int[btn_num];
         for (int i = 0; i < btn_num; i++) {
-            func_x[i] = (width / btn_num) * i + width / btn_num;
+            func_x[i] = (width / btn_num) * i;// + width / btn_num;
         }
-        menuPanel.add(exitButton(width, height, func_x[5]));
-        menuPanel.add(projectMenu(width,height,0));
+        menuPanel.add(exitButton(i_width, i_height, func_x[5]));
+        menuPanel.add(projectMenu(i_width,i_height,func_x[0]));
+        menuPanel.add(openProjectMenu(i_width,i_height,func_x[1]));
 
         return menuPanel;
     }
     private Component exitButton(int width, int height, int x){
         JButton button = new JButton();
         button.setLayout(null);
-        int button_width = (int) (width * 0.08);
         int button_height = height;
         int y = 0;
-        button.setBounds(windowWidth-button_width, y, button_width, button_height);
-        button.setBackground(Window.button_off_c);
-        button.setBorderPainted(true);
+        button.setBounds(windowWidth-width, y, width, button_height);
+        button.setBackground(Window.menu_panel_c);
+        button.setBorderPainted(false);
         JLabel label = new JLabel();
         label.setText("Exit");
-        label.setBounds(0, 0, button_width, button_height);
+        label.setBounds(0, 0, width, button_height);
         label.setHorizontalAlignment(SwingConstants.CENTER);
         label.setVerticalAlignment(SwingConstants.CENTER);
         label.setFont(new Font(Window.font, Font.BOLD, (int) (button_height * 0.6)));
@@ -169,13 +173,13 @@ public class ProjectWindow {
     private Component projectMenu(int width, int height, int x){
         JMenu menu = new JMenu();
         // menu.setMenuLocation(x, height);
-        int btn_width = (int) (width*0.08);
-        menu.setBounds(0,0,btn_width, height);
+        menu.setBounds(0,0,width, height);
         menu.setBackground(Window.button_hover);
         menu.setText("Project");
-        JMenuItem newMenuItem = new JMenuItem("New");
+        JMenuItem newMenuItem = new JMenuItem("New         "); //""
+        newMenuItem.setHorizontalAlignment(SwingConstants.LEFT);
         newMenuItem.setBackground(Window.menu_item_c);
-        newMenuItem.setSize(btn_width,height);
+        newMenuItem.setSize(width,height);
         newMenuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -184,8 +188,9 @@ public class ProjectWindow {
         });
         menu.add(newMenuItem);
         JMenuItem saveMenuItem = new JMenuItem("Save");
+        saveMenuItem.setHorizontalAlignment(SwingConstants.LEFT);
         saveMenuItem.setBackground(Window.menu_item_c);
-        saveMenuItem.setSize(btn_width,height);
+        saveMenuItem.setSize(width,height);
         saveMenuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -193,17 +198,75 @@ public class ProjectWindow {
             }
         });
         menu.add(saveMenuItem);
-        JMenuItem openMenuItem = new JMenuItem("Open");
-        saveMenuItem.setBackground(Window.menu_item_c);
-        saveMenuItem.setSize(btn_width,height);
-        saveMenuItem.addActionListener(new ActionListener() {
+        JMenuItem deleteMenuItem = new JMenuItem("Delete");
+        deleteMenuItem.setBackground(Window.menu_item_c);
+        deleteMenuItem.setSize(width,height);
+        deleteMenuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //Create a drop down/menu of existing projects
+                deleteProject(p_id);
+                frame.dispose();
+                new StartWindow();
             }
         });
+        menu.add(deleteMenuItem);
 
         return menu;
+    }
+    private Component openProjectMenu(int width, int height, int x){
+        openProjectM = new JMenu();
+        openProjectM.setText("Open Project");
+        openProjectM.setBounds(x,0, width, height);
+        openProjectM.setBackground(Window.menu_item_c);
+        fillOpenProjectMenu();
+        return openProjectM;
+    }
+    private void populateOpenProjectMap(){
+        ConnectionSettings settings = new ConnectionSettings();
+        try {
+            Connection conn = settings.getDatabaseConnection();
+            String sql = "SELECT p_id, p_name FROM projects";
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while(resultSet.next()){
+                openProjectMap.put(resultSet.getString("p_name"), resultSet.getInt("p_id"));
+            }
+        } catch (SQLException e){
+            throw new RuntimeException();
+        }
+    }
+    private void fillOpenProjectMenu(){
+        populateOpenProjectMap();
+        for(Map.Entry entry: openProjectMap.entrySet()){
+            String key = String.valueOf(entry.getKey());
+            int value = (int) (entry.getValue());
+            JMenuItem item = new JMenuItem(key);
+            item.setBackground(Window.menu_item_c);
+            item.setBorderPainted(false);
+            openProjectM.add(item);
+            item.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    saveTabToDatabase();
+                    new ProjectWindow(new Project(value));
+                    frame.dispose();
+                }
+            });
+        }
+    }
+    private void deleteProject(int p_id){
+        ConnectionSettings settings = new ConnectionSettings();
+        try {
+            Connection conn = settings.getDatabaseConnection();
+            String sql = "DELETE FROM `guitartab`.`projects` WHERE  `p_id`=" + p_id;
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while(resultSet.next()){
+                openProjectMap.put(resultSet.getString("p_name"), resultSet.getInt("p_id"));
+            }
+        } catch (SQLException e){
+            throw new RuntimeException();
+        }
     }
     private Component functionsPanel() {
         functionsPanel.setLayout(null);
@@ -421,22 +484,28 @@ public class ProjectWindow {
     }
     private void TabListToString() {
         StringBuffer str_tab_buff = new StringBuffer();
-        for (int y = 0; y < 7; y++) {
-            for (int x = 0; x < tab.size(); x++) {
-                String current_val = tab.get(x).get(y);
-                str_tab_buff.append(current_val);
-            }
-            str_tab_buff.append("\n");
+        int row_val = 44;
+        int current_row = 0;
+        int end_idx = 0;
+        while(end_idx< tab.size()){
+            int begin_idx =  row_val * current_row;
+            end_idx = begin_idx+row_val;
+            if (end_idx >= tab.size()) end_idx = tab.size();
+           str_tab_buff.append(LimitedTabListToString(begin_idx, end_idx));
+           str_tab_buff.append("\n");
+
+            current_row++;
         }
-        //System.out.println(str_tab_buff); // for debugging
         str_tab = str_tab_buff.toString();
     } //Function used to display the Tablature in the frame
     private String LimitedTabListToString(int begin_col, int end_col) {
         StringBuffer str_tab_buff = new StringBuffer();
         List<List<String>> limited_tab = new LinkedList<>();
-        for (int i = begin_col; i<= end_col; i++){
+        for (int i = begin_col; i< end_col; i++){
             limited_tab.add(tab.get(i));
+            System.out.println(limited_tab);
         }
+        System.out.println(limited_tab);
         for (int y = 0; y < 7; y++) {
             for (int x = 0; x < limited_tab.size(); x++) {
                 String current_val = limited_tab.get(x).get(y);
@@ -444,37 +513,33 @@ public class ProjectWindow {
             }
             str_tab_buff.append("\n");
         }
-        return str_tab_buff.toString();
+        //str_tab = str_tab_buff.toString();
+        return  str_tab_buff.toString();
     } //Used to get one panel row of tab
-
-    private JButton projectButton(int width, int height, int x){
-        JButton button = new JButton();
-        button.setLayout(null);
-        int button_width = (int) (width * 0.06);
-        int button_height = height;
-        int y = (height - button_height) / 2;
-        button.setBounds(0, 0, button_width, button_height);
-        button.setBackground(Window.button_off_c);
-        button.setBorderPainted(true);
-        JLabel label = new JLabel();
-        label.setText("Project");
-        label.setBounds(0, 0, button_width, button_height);
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        label.setVerticalAlignment(SwingConstants.CENTER);
-        label.setFont(new Font(Window.font, Font.BOLD, (int) (button_height * 0.4)));
-        button.add(label);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-
-
+    public String convertTabToDatabaseString(){
+        StringBuffer stringBuffer = new StringBuffer();
+        for(int x = 0; x<tab.size(); x++){
+            for(int y = 0; y<7; y++){
+                stringBuffer.append(tab.get(x).get(y) + ",");
             }
-        });
-        return button;
+        }
+        System.out.println(stringBuffer);
+        return String.valueOf(stringBuffer);
     }
+    public void saveTabToDatabase(){
+        ConnectionSettings settings = new ConnectionSettings();
+        try {
+            Connection conn = settings.getDatabaseConnection();
+            PreparedStatement statement = conn.prepareStatement("UPDATE `guitartab`.`projects` SET `p_cvs_val`= ? WHERE `p_id` = ?");
+            statement.setString(1, convertTabToDatabaseString());
+            statement.setString(2, String.valueOf(p_id));
+            statement.executeUpdate();
+            System.out.println("Update executed: " + statement);
 
-
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
     private void setButtons(Fretboard fretboard, JPanel panel) {
         int b_diamater = (int) (fretboardPanelHeight / 7.3);
         for (int y = 0; y < 6; y++) {
@@ -510,6 +575,10 @@ public class ProjectWindow {
         else tab.get(currently_edited).set(6, "^^");
         TabListToString();
     }
+    private void validateScrollBar(){
+
+        scrollFrame.getVerticalScrollBar().setValue(scrollBar_val);
+    }
     //Modified Classes
     private class FretboardPanel extends JPanel {
         @Override
@@ -543,6 +612,7 @@ public class ProjectWindow {
         private boolean inKey = false;
         private int fret;
         private final int string;
+
         private final String audio_file;
 
         public RoundButton(int fret, int string, boolean inKey, String audio_file) {
@@ -574,7 +644,6 @@ public class ProjectWindow {
             int diameter = Math.min(getWidth(), getHeight());
             return diameter;
         }
-
         @Override
         public void paintComponent(Graphics g) {
             int radius = getDiameter() / 2;
@@ -616,49 +685,27 @@ public class ProjectWindow {
             g.fillOval(getWidth() / 2 - radius, getHeight() / 2 - radius, radius * 2, radius * 2);
         }
         public boolean isInKey() {return inKey;}
+
         public void setInKey(boolean inKey) {this.inKey = inKey;}
-
         public int getFret() {return fret;}
-        public void setFret(int fret) {this.fret = fret;}
 
+        public void setFret(int fret) {this.fret = fret;}
     }
     class ValidateThread implements Runnable {
         @Override
         public void run() {
+            scrollBar_val = scrollFrame.getVerticalScrollBar().getValue();
             TabListToString();
                 if (tablatureTextArea().getText() != str_tab) {
+
                     validatePointer();
                     tablatureTextArea().setText(str_tab);
                     tablatureTextArea().repaint();
+                    validateScrollBar();
+                    scrollFrame.getVerticalScrollBar().setValue(scrollBar_val);
             }
         }
     } // interface runnable include in Crit C
-
-    public String convertTabToDatabaseString(){
-        StringBuffer stringBuffer = new StringBuffer();
-        for(int x = 0; x<tab.size(); x++){
-            for(int y = 0; y<7; y++){
-                stringBuffer.append(tab.get(x).get(y) + ",");
-            }
-        }
-        System.out.println(stringBuffer);
-        return String.valueOf(stringBuffer);
-    }
-    public void saveTabToDatabase(){
-        ConnectionSettings settings = new ConnectionSettings();
-        try {
-            Connection conn = settings.getDatabaseConnection();
-            PreparedStatement statement = conn.prepareStatement("UPDATE `guitartab`.`projects` SET `p_cvs_val`= ? WHERE `p_id` = ?");
-            statement.setString(1, convertTabToDatabaseString());
-            statement.setString(2, String.valueOf(p_id));
-            statement.executeUpdate();
-            System.out.println("Update executed: " + statement);
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public static void main(String[] args) {
         Project project = new Project(1);
         new ProjectWindow(project);
