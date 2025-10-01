24f5f268fba3bb7dc5fc6a730b525133dc788fba
MatrixPie <matthew.kowalczyk@gmail.com>
2025-10-01 22:35:10 +0200

    fixed files hopefully

--- src/GuitarTabMaker/ConnectionSettings.java
+++ src/GuitarTabMaker/ConnectionSettings.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionSettings {
-    private static final String URL = "jdbc:mariadb://127.0.0.1:3306/guitartab?serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String URL ="jdbc:mysql://127.0.0.1:3306/guitartab?serverTimezone=UTC"; // "jdbc:mysql://uymht1l8wfretrel:Rmg4qIx0YJpN1qhwYRW7@bv3szmnzzx3y6m4bbe3w-mysql.services.clever-cloud.com:3306/bv3szmnzzx3y6m4bbe3w"; //
+    private static final String USER = "root";//""uymht1l8wfretrel";
+    private static final String PASSWORD = "root";//""Rmg4qIx0YJpN1qhwYRW7";
 
     public static Connection getDatabaseConnection() throws SQLException {
         return DriverManager.getConnection(URL,USER, PASSWORD);
